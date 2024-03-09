99bdd62dceaf9e55939e38afac40e00a1cd6c77b
sparkkkd <62719004+sparkkkd@users.noreply.github.com>
2024-03-09 17:31:44 +0300

    fix SSL x2

--- src/redux/slices/goodsSlice.js
+++ src/redux/slices/goodsSlice.js
@@ -20,7 +20,7 @@ export const fetchPagination = createAsyncThunk(
 	'goods/fetchPagination',
 	async ({ offset, limit }, { rejectWithValue }) => {
 		try {
-			const goodsIds = await fetch('https://api.valantis.store/', {
+			const goodsIds = await fetch('https://api.valantis.store:4100/', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
@@ -34,7 +34,7 @@ export const fetchPagination = createAsyncThunk(
 
 			const { result } = await goodsIds.json()
 
-			const goods = await fetch('https://api.valantis.store/', {
+			const goods = await fetch('https://api.valantis.store:4100/', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
@@ -58,7 +58,7 @@ export const fetchBrands = createAsyncThunk(
 	'goods/fetchBrands',
 	async (_, { rejectWithValue }) => {
 		try {
-			const getAllBrands = await fetch('https://api.valantis.store/', {
+			const getAllBrands = await fetch('https://api.valantis.store:41000/', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
